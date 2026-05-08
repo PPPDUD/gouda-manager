2bb3da9d69805774bed50eb1d49d8b00a1e0461e
PPPDUD <107440101+PPPDUD@users.noreply.github.com>
2026-05-08 11:22:45 -0400

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/bash
 cd "$(dirname "$0")"
 rm -rf * .git
-rmdir .
+dir_to_remove=$PWD
+cd ..
+rmdir $dir_to_remove
 cd /usr/local/bin
 rm gouda-config gouda-install gouda-uninstall gouda-upgrade gouda-chkroot
