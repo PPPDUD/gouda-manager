f73db1c5757597184691af5f40a715643ccba12f
PPPDUD <107440101+PPPDUD@users.noreply.github.com>
2026-05-08 11:18:08 -0400

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 chmod +x *.sh
-cp gouda-config gouda-install gouda-uninstall gouda-upgrade /usr/local/bin/
+cp gouda-config gouda-install gouda-uninstall gouda-upgrade gouda-chkroot /usr/local/bin/
 chmod +x /usr/local/bin/gouda-*
 echo Gouda installed to /usr/local/bin!
--- uninstall.sh
+++ uninstall.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/bash
 cd "$(dirname "$0")"
-rm -rf *
+rm -rf * .git
+rmdir .
 cd /usr/local/bin
-rm gouda-config gouda-install gouda-uninstall gouda-upgrade
+rm gouda-config gouda-install gouda-uninstall gouda-upgrade gouda-chkroot
