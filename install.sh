19eaa19dae8fed0350cc16e867874265a64a24e7
PPPDUD <107440101+PPPDUD@users.noreply.github.com>
2026-05-08 11:06:08 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd "$(dirname "$0")"
-chmod +x gouda-* *.sh
+chmod +x *.sh
 cp gouda-config gouda-install gouda-uninstall gouda-upgrade /usr/local/bin/
+chmod +x /usr/local/bin/gouda-*
 echo Gouda installed to /usr/local/bin!
